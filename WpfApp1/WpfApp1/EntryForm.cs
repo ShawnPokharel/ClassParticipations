682a2f1823426a893c9d1da12545f0ad209ef3b6
Shawn Pokharel <Shawn.Pokharel-1@ou.edu>
2020-01-29 11:43:08 -0600

    Almost done with participation need to figure out listbox which is listbox.items.add(whatever) and entryform.

---
+++ WpfApp1/WpfApp1/EntryForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class EntryForm
+    {
+        public string Name { get; set; }
+        //public string LastName { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+            Name = string.Empty;
+           // LastName = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+        public EntryForm(string name, string address, int zipCode)
+        {
+            Name = name;
+            
+            Address = address;
+            ZipCode = zipCode;
+
+
+        }
+        public override string ToString()
+        {
+            return $"{Name} lives at {Address} at the zipcode {ZipCode}";
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbxFirstName.Text = String.Empty;
+            tbxLastName.Text = String.Empty;
+            tbxAddress.Text = String.Empty;
+            tbxZipCode.Text = String.Empty;
+
+            
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String firstName, lastName, address, zipCode;
+
+            
+
+            firstName = tbxFirstName.Text;
+            lastName = tbxLastName.Text;
+            address = tbxAddress.Text;
+            zipCode = tbxZipCode.Text;
+
+            String name = firstName + " " + lastName;
+            EntryForm myEntryForm = new EntryForm();
+
+            ListBox listBox = new ListBox();
+
+           //ListBox.
+
+
+           
+
+            //String message = $"{firstName} + {lastName} + {address} + {zipCode}";
+            //MessageBox.Show(message);
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+    
+}
